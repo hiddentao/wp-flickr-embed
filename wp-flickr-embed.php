0e5250ea9f3e977f90521345e1d39ed63368fa97
Ramesh Nair <ram@hiddentao.com>
2012-11-05 12:52:33 +0800

    Clean up plugin description.

--- wp-flickr-embed.php
+++ wp-flickr-embed.php
@@ -2,8 +2,7 @@
 /*
 Plugin Name: Wordpress Flickr Embed
 Plugin URI: http://factage.com/yu-ji/tag/wp-flickr-embed
-Description: Insert Flickr images into your post using an interactive popup, launched from the visual editor toolbar. Based on
-wordpress-media-flickr plugin by yu-ji.
+Description: Insert Flickr images into your post using an interactive popup, launched from the visual editor toolbar.
 Author: Ramesh Nair
 Version: 1.0.0
 Author URI: http://hiddentao.com
